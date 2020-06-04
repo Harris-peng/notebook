bae1e7b86005a414d45aed7ede2e68e6cd186288
harris-peng <dangpenghao@outlook.com>
2020-06-04 18:42:26 +0800

    :art: 增加promise

--- book/test.js
+++ book/test.js
@@ -19,3 +19,53 @@ function converter(obj) {
   })
   return res;
 }
+function sort(a, b) {
+  return (a - b) > 0
+}
+function swap (list, i , j) {
+  let a = list[i]
+  list[i] = list[j];
+  list[j] = a;
+  return list
+}
+function maopao(list, sort) {
+  for (let i = 0; i<list.length; i++) {
+    for(let j = i + 1;  j < list.length; j++ ) {
+      if (sort(list[i], list[j])) swap(list, i, j);
+    }
+  }
+  return list;
+}
+function quicksort(arr = [], L = 0, R = arr.length-1) {
+  if (L>=R) return
+  const val = arr[~~((L+R)/2)];
+  let i = L,j = R;
+  while (i <= j) {
+    while (sort(arr[i], val) && i <= j) {
+      i++
+    }
+    while (sort(val, arr[j]) && j >i) {
+      j--
+    }
+    if (i <= j) {
+      swap(arr, i++, j--)
+    }
+  }
+  quicksort(arr, L, i-1)
+  quicksort(arr, i, R)
+  return arr;
+}
+function co(gen) {
+  gen = gen();
+  return new Promise((resolve, reject) => {
+    function next(val = undefined) {
+       const res  = gen.next(val)
+      if (res.done) return resolve(res.value);
+      res.value.then(res => {
+        next(res)
+      }, reject)
+    }
+    next(undefined);
+  })
+
+}
